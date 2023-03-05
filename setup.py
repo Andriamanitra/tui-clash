88f67d4769c3d20d7a08864bba81a8374e2d6105
4atj <104042557+4atj@users.noreply.github.com>
2023-03-05 11:02:11 +0100

    add setup.py (#1)
    * added a setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+    name = "tui-clash",
+    version = "0.0.1",
+    url = "https://github.com/Andriamanitra/tui-clash",
+    classifiers = [
+        "Environment :: Console",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Operating System :: Microsoft :: Windows :: Windows 11",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+
+    python_requires = ">=3.7",
+    requires = ["textual"],
+
+    packages = ["tui_clash"],
+    package_dir = {'tui_clash': 'client'},
+    package_data = {'tui_clash': ['style.css']},
+
+    entry_points = {
+        'console_scripts': [
+            'tui-clash = tui_clash.client:main',                  
+        ],
+    },
+)
